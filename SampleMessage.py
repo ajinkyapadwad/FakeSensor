ddc59f3512c634df13b0edaa39e36a339cd2d22c
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-02-18 21:38:37 -0500

    Update SampleMessage.py

--- SampleMessage.py
+++ SampleMessage.py
@@ -1,7 +1,4 @@
 
-#import java.util.Arrays;
-
-#import com.owlplatform.common.util.NumericUtils;
 import time
 
 class SampleMessage :
@@ -10,7 +7,7 @@ class SampleMessage :
   
   MESSAGE_TYPE = 6
   
-  PHYSICAL_LAYER_UNDEFINED = 0xFF # convert to byte.
+  PHYSICAL_LAYER_UNDEFINED = bytes(0xFF) # convert to byte.
   
   PHYSICAL_LAYER_ALL = 0
 
@@ -26,9 +23,8 @@ class SampleMessage :
   	return self.physicalLayer
 
   def setPhysicalLayer(physicalLayer):
-    try:
-      physicalLayer == PHYSICAL_LAYER_ALL
-    except: IllegalArgumentException("Invalid physical layer type. " + PHYSICAL_LAYER_ALL + " is reserved for filtering only.")
+    if physicalLayer == PHYSICAL_LAYER_ALL :
+      raise ValueError("Invalid physical layer type. " + PHYSICAL_LAYER_ALL + " is reserved for filtering only.")
     self.physicalLayer = physicalLayer    
  
   def SampleMessage() :
@@ -40,35 +36,37 @@ class SampleMessage :
   def getLengthPrefixSensor() :
     # physicalLayer, devId, recvId, timestamp, rssi
     length = 1 + DEVICE_ID_SIZE * 2 + 8 + 4
-    if self.sensedData is not none :
+    if self.sensedData is not None :
       length += len(self.sensedData)
     return length
 
   def getLengthPrefixSolver() :
     # physicalLayer, messageId, devId, recvId, timestamp, rssi
     length = 2 + DEVICE_ID_SIZE * 2 + 8 + 4;
-    if self.sensedData is not none :
+    if self.sensedData is not None :
       length += this.sensedData.length    
     return length
+
+    #deviceId = bytes()
 
   def getDeviceId() :
     return self.deviceId
 
   def setDeviceId(deviceId) :
-    try : deviceId is none
-    except : RuntimeException("Device ID cannot be null.")
+    try : deviceId is None
+    except : RuntimeError("Device ID cannot be null.")
     try : deviceId.length is not DEVICE_ID_SIZE
-    except : RuntimeException("Device ID must be" + int(DEVICE_ID_SIZE)+ " bytes long.")
+    except : RuntimeError("Device ID must be" + int(DEVICE_ID_SIZE)+ " bytes long.")
     self.deviceId = deviceId
 
   def getReceiverId() :
     return self.receiverId
 
   def setReceiverId(receiverId) :
-    try : receiverId is none
-    except : RuntimeException("Receiver ID cannot be null.")	
+    try : receiverId is None
+    except : RuntimeError("Receiver ID cannot be null.")	
     try : receiverId.length != DEVICE_ID_SIZE
-    except : RuntimeException("Receiver ID must be "+int(DEVICE_ID_SIZE)+" bytes long.")
+    except : RuntimeError("Receiver ID must be "+int(DEVICE_ID_SIZE)+" bytes long.")
     self.receiverId = receiverId;
 
   def getReceiverTimeStamp() :
@@ -89,49 +87,24 @@ class SampleMessage :
   def setSensedData(sensedData) :
     self.sensedData = sensedData
 
-    
-    	#-----------------------------------------
-    	public String toString() 
-      {
-    	StringBuffer sb = new StringBuffer()
-    	sb.append("Sample (").append(this.getPhysicalLayer())
-    	sb.append(", ")
-    	sb.append(NumericUtils.toHexString(this.getDeviceId()))
-    	sb.append(", ")
-    	sb.append(NumericUtils.toHexString(this.getReceiverId()))
-    	sb.append("): ")
-    	sb.append(this.getRssi())
-    	sb.append(" @ ")
-    	sb.append(this.getReceiverTimeStamp())
-    	if (this.getSensedData() != null) 
-    	{
-    		sb.append(" [").append(NumericUtils.toHexString(this.getSensedData()));
-    		sb.append(']')
-    	}		
-    	return sb.toString()
-    	}
-    	#-----------------------------------------
-    """
+  # This one here --
+  def _str_():
+    strlist = []
+    strlist.append('Sample' + self.getPhysicalLayer()+", "+chr(self.getDeviceId())+", "+chr(self.getReceiverId())+"): "+self.getRssi()+" @ "+self.getReceiverTimeStamp())
+    if self.getSensedData() is not None :
+   		strlist.append(" ["+chr(self.getSensedData())+']')		
+    out_str = ''.join(str_list)
+    return out_str
+  #-----------------------------------------------------------
   def getCreationTimestamp() :
     return self.creationTimestamp
 
   def getTestMessage() : 
     message = SampleMessage()
-    a =[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ]
+    a = bytes([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ])
     message.setDeviceId(a)
     message.setReceiverId(a) # needs new instance of a[] ?
-    message.setPhysicalLayer(byte(1)) # byte of ...
+    message.setPhysicalLayer(bytes(1)) # byte of ...
     message.setRssi(float(-50))
-    message.setSensedData(0, 0) # passs arrray of byte as argument ...
+    message.setSensedData(bytes([0, 0])) # passs arrray of byte as argument ...
     return message
-"""
-  def clone() throws CloneNotSupportedException 
-   {
-   		SampleMessage clone = (SampleMessage) super.clone()
-   		if (this.sensedData != null) 
-   		{ 
-   			clone.setSensedData(Arrays.copyOf(this.sensedData, this.sensedData.length))
-   		}
-   		return clone
-   }
-"""
