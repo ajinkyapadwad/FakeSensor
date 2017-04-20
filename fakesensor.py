3d82607ce3bddc813285601cf13d944db781e38c
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-04-20 15:36:17 -0400

    new

--- fakesensor.py
+++ fakesensor.py
@@ -13,91 +13,99 @@ import sys
 from termcolor import colored
 
 # ------------------- CONNECTION SETUP --------------------------
-try :
-	# take command line arguments :
-	try: 
-		host = sys.argv[1]
-		port = sys.argv[2]
+def main(host,port):
+
+	# sample data values 
+	message_length = 0x2f 
+	physical_layer = 1 
+	timestamp =  int(round(time.time()*1000))
+	RSSI = -50.0
+	sensor_data1 = 0xab
+	sensor_data2 = 0xba
+	
+	try :
+		# create new socket for TCP / IP connection
+		print colored("\n	connecting to the Aggregator server ...", 'green')
+		time.sleep(1)
+
+		
+		NewSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 	# ( IPv4, socket constant )
+		NewSocket.connect((host, port))	# attach socket to remote address
+
+		
+
+		print colored("	Connection Successful !", 'green')
+
+
+	 # ------------------ HANDSHAKE -------------------------------------
+		print colored('	Handshake initiated ...\n', 'green')
+		time.sleep(1)
+		handshake = (21,'GRAIL sensor protocol', 0, 0)
+
+		packer = struct.Struct('!'+'I 21s b b')				# declare a new struct object
+		packed_data = packer.pack(*handshake)	
+
+		#time.sleep(1)
+
+		print colored('	Sending Handkshake Message..', 'cyan')
+
+		data = NewSocket.recv(36)
+		print "Received:",data
+
+		NewSocket.sendall(packed_data)
+		print 'Sent    :',packed_data
+
+		print colored('\n\n 	Handshake Complete.','green')
+
+	 # ------------------ SENDING SAMPLES -------------------------------
+
+		print colored('\n 	Now sending sample sensor data...', 'blue')
+		time.sleep(1)
+		
+
+		SensorData = ( message_length, physical_layer, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 ,16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, timestamp, RSSI , sensor_data1, sensor_data2 )
+		packer = struct.Struct('!'+'i B 16B 16B Q f 2B')
+
+		packed_data = packer.pack(*SensorData)
+
+		i=1
+		while 1:
+			NewSocket.sendall(packed_data)
+			print 'SENT: Sample -', i
+			i=i+1
+			time.sleep(1)
+
+			# NewSocket.close()
+			# sys.exit()
+
 	except:
-		print colored("	Please input the host / port name.\n 	Syntax: python filename.py hostname port", 'red')
-		print colored("\n\n Default : \n 	Hostname - localhost \n 	port - 7007 ", 'blue')
-	# host = 'localhost'	# IPv4 address, localhost for this program
-	# port = 7007			# port number	
-
-	# create new socket for TCP / IP connection
-	print colored("\n	connecting to the Aggregator server ...", 'green')
-	time.sleep(1)
-
-	NewSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 	# ( IPv4, socket constant )
-	NewSocket.connect((argv[1], argv[2]))	# attach socket to remote address
-
-	print a
-
-	print colored("Connection Successful !", 'green')
-
-	NewSocket.close()
-	sys.exit()
-# ------------------ HANDSHAKE -------------------------------------
-	print colored('Handshake initiated ...', 'green')
-
-	handshake = (21,'GRAIL sensor protocol', 0, 0)
-
-	packer = struct.Struct('!'+'I 21s b b')				# declare a new struct object
-	packed_data = packer.pack(*handshake)	
-
-	#time.sleep(1)
-
-	print 'Sending Handkshake Message..'
-
-	data = s.recv(36)
-	print "Received:",data
-
-	s.sendall(packed_data)
-	print 'Sent    :',packed_data
-
-	print 'Handshake Complete.'
-
-
-except:
-	ErrorText = colored('	One or more errors have occurred !\n', 'red')
-	print(ErrorText)
-	NewSocket.close()
-	sys.exit()
+		ErrorText = colored('\n\n 	One or more errors have occurred !\n', 'red')
+		print(ErrorText)
+		print " 	socket disconnected\n"
+		#NewSocket.close()
+		sys.exit(0)
 
 
 
+if __name__ == '__main__':
+	try:
+		# take command line arguments :
+		host = sys.argv[1]
+		port = int(sys.argv[2]) 
+	
+		if message_length(sys.argv) is 3:
+			print "		Host:" , host
+			print "		Port:" , port
+			main(host,port)	
 
-
-# print"Sleep 10 seconds.."
-# time.sleep(10)
-
-# ---------------------------------------------------------------------------------------------------------
-
-print 'Now sending sample sensor data...'
-
-LEN = 0x2f  # 4 byte value  -- I
-PHY = 1 # 1 byte value -- b ,
-#DEV_ID = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12, 13, 14, 15, 16 ]
-#RECV_ID = [ 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
-timestamp1 =  int(round(time.time()*1000))
-#timestamp2 = str(timestamp1)
-RSSI = -50.0
-Data1 = 0xab
-Data2 = 0xba
-
-SensorData = ( LEN, PHY, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 ,16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, timestamp1, RSSI , Data1, Data2 )
-packer = struct.Struct('!'+'i B 16B 16B Q f 2B')
-
-# SensorData = (47, 1)
-# packer = struct.Struct('!'+'H b')
+	except:
+		print colored("	Please input appropriate host name and port number.\n",'red') 	
+		print colored("	Syntax: python <filename.py>  <hostname>  <portnumber>", 'yellow')
+		print colored("\n\n Try Default : \n 	Hostname - localhost \n 	port - 7007 ", 'blue')
+		sys.exit()
+	
 
 
 
-packed_data = packer.pack(*SensorData)
+# ----------------- END --------------------------------------------------------------------------
 
-i=1
-while 1:
-	s.sendall(packed_data)
-	print 'SENT: Sample -', i
-	i=i+1
-	time.sleep(1)
