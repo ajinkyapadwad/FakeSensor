de03d62467b6952dff5b7a9629322a3115688bb1
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-04-20 16:28:50 -0400

    new

--- fakesensor.py
+++ fakesensor.py
@@ -1,6 +1,10 @@
 """
 OWL PLATFORM @ RUTGERS WINLAB
 PYTHON LIBRARY : FAKESENSOR
+AUTHOR : AJINKYA PADWAD
+
+MAIN REFERENCE : https://git.owlplatform.com/wiki/index.php/Category:GRAIL_RTLS_v3_Documentation
+
 """
 
 # -------------------- DEPENDENCIES -----------------------------
@@ -8,24 +12,35 @@ import socket
 import struct
 import binascii
 import time
-import array
 import sys
 from termcolor import colored
 
 # ------------------- CONNECTION SETUP --------------------------
 def main(host,port):
 
-	# sample data values 
-	message_length = 0x2f 
-	physical_layer = 1 
-	timestamp =  int(round(time.time()*1000))
+	# handshake message values
+	StringLength = 21
+	ProtocolMessage = 'GRAIL sensor protocol'
+	Version = 0
+	ReservedBits = 0
+
+	# sample sensor data values 
+	MessageLength = 0x2f 
+	PhysicalLayer = 1 
+	# DeviceID = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+	# ReceiverID =[16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+	DeviceID = '\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16'
+	ReceptionTime =  int(round(time.time()*1000))
 	RSSI = -50.0
-	sensor_data1 = 0xab
-	sensor_data2 = 0xba
+	SensorData1 = 0xab
+	SensorData2 = 0xba
 	
+	MessageBuffer = 50
+
+
 	try :
 		# create new socket for TCP / IP connection
-		print colored("\n	connecting to the Aggregator server ...", 'green')
+		print colored("\n	Connecting to the Aggregator server ...", 'green')
 		time.sleep(1)
 
 		
@@ -38,40 +53,44 @@ def main(host,port):
 
 
 	 # ------------------ HANDSHAKE -------------------------------------
+		
+	 	HandshakeMessage = (StringLength, ProtocolMessage, Version, ReservedBits)
+
 		print colored('	Handshake initiated ...\n', 'green')
-		time.sleep(1)
-		handshake = (21,'GRAIL sensor protocol', 0, 0)
-
-		packer = struct.Struct('!'+'I 21s b b')				# declare a new struct object
-		packed_data = packer.pack(*handshake)	
-
-		#time.sleep(1)
-
-		print colored('	Sending Handkshake Message..', 'cyan')
-
-		data = NewSocket.recv(36)
-		print "Received:",data
-
-		NewSocket.sendall(packed_data)
-		print 'Sent    :',packed_data
-
-		print colored('\n\n 	Handshake Complete.','green')
-
-	 # ------------------ SENDING SAMPLES -------------------------------
-
-		print colored('\n 	Now sending sample sensor data...', 'blue')
 		time.sleep(1)
 		
 
-		SensorData = ( message_length, physical_layer, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 ,16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, timestamp, RSSI , sensor_data1, sensor_data2 )
-		packer = struct.Struct('!'+'i B 16B 16B Q f 2B')
+		Packer = struct.Struct('!'+'I 21s b b')				# declare a new struct object
+		DataPacket = Packer.pack(*HandshakeMessage)	
 
-		packed_data = packer.pack(*SensorData)
+		
+		print colored('	Sending handkshake message..', 'cyan')
+		time.sleep(1)
+
+		Received = NewSocket.recv(MessageBuffer)
+		print "Received:",Received
+
+		NewSocket.sendall(DataPacket)
+		print 'Sent    :',DataPacket
+
+		print colored('\n\n 	Handshake complete.','green')
+
+	 # ------------------ SENDING SAMPLES -------------------------------
+
+		print colored('\n 	Now sending sensor data...', 'blue')
+		time.sleep(1)
+		
+
+		SensorData = ( MessageLength, PhysicalLayer, DeviceID, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, ReceptionTime, RSSI , SensorData1, SensorData2 )
+		Packer = struct.Struct('!'+'i B 16B 16B Q f B B')
+
+		DataPacket = Packer.pack(*SensorData)
 
 		i=1
 		while 1:
-			NewSocket.sendall(packed_data)
+			NewSocket.sendall(DataPacket)
 			print 'SENT: Sample -', i
+			#print '	[',binascii.hexlify(DeviceID),':', binascii.hexlify(ReceiverID),' ] @', RSSI, ':- ', ReceptionTime
 			i=i+1
 			time.sleep(1)
 
@@ -92,8 +111,8 @@ if __name__ == '__main__':
 		# take command line arguments :
 		host = sys.argv[1]
 		port = int(sys.argv[2]) 
-	
-		if message_length(sys.argv) is 3:
+
+		if len(sys.argv) is 3:
 			print "		Host:" , host
 			print "		Port:" , port
 			main(host,port)	
